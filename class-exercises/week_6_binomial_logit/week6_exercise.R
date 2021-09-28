a34ced80b4e902f4547fe7daa9cd74d3e930a1e2
gg3 <gg3@vt.edu>
2021-09-28 10:47:40 -1000

    added week 6 exercise

---
+++ class-exercises/week_6_binomial_logit/week6_exercise.R
@@ -0,0 +1,5 @@
+# This week, we will try to predict whether a wine is white or red based on other chemical and perceptual characteristics.
+# Download the datasets here: https://archive.ics.uci.edu/ml/datasets/wine+quality
+library(tidyverse)
+
+# Hint: you should create a new variable before merging the two datasets.
