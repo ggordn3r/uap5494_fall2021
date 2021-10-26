b9bd118a89e1bcafe50795b3e660e55cf6d73681
gg3 <gg3@vt.edu>
2021-10-26 12:46:18 -1000

    post time series example

---
+++ class-exercises/week_10_time_series/week_10_example.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+data <- austres
+
+# Plot the data. Does it make sense to log transform your variable? Why or why not?
+
+# Test for autocorrelation, partial autocorrelation, and non-stationarity.
+# Interpret the results in a comment.
+
+# calculate and plot first differences.
+# Compare this to the raw data in a comment. When does it make sense to use differences instead of raw data?
+
+# decompose the data and plot the figures
+
+# create an ARIMA model from the data
+
+# forecast the next 2 years of your variable
+
+# plot the forecast
+# Interpret the results in a comment. Why is the range so large?
+
+# Validate your forecast with a Box test.
+# Interpret the test output in a comment.
+
+
+
+# Further practice (optional)
+# NYC shooting dataset: 
+# https://www.kaggle.com/thaddeussegura/new-york-city-shooting-dataset
+# French trains dataset:
+# https://github.com/rfordatascience/tidytuesday/tree/master/data/2019/2019-02-26
